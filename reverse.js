a608c6cfaa12baa874dac2b0fd9ab100eeb1c68b
Roshika Bajracharya <roshikabajracharyaa@gmail.com>
2020-02-18 11:19:08 +0545

    reverse is added

---
+++ reverse.js
@@ -0,0 +1,14 @@
+function string_reverse(str) {
+  return str
+    .split("")
+    .reverse()
+    .join("");
+}
+console.log(string_reverse("print"));
+
+console.log(
+  "upekkhA"
+    .split("")
+    .reverse()
+    .join("")
+);
